fd513005fa445b390baa106f668cceaf6994438c
RPSC <audtjsdua@naver.com>
2022-06-06 04:04:35 +0900

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,10 +57,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'MsYeom\'s Blog',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/home.png',
         },
         items: [
           {
